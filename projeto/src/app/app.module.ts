623fe57dab5cff29b1e7ba401577323a2f15781f
Daniel Guimarães de Faria <164964227+Daniel-Guimaraes-de-Faria@users.noreply.github.com>
2025-03-24 22:44:19 +0000

    Recriando Projeto

---
+++ projeto/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserModule,
+    MatCardModule,
+    MatToolbarModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
